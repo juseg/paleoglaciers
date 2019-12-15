38b1a10971c861a17334aa60ff1fc8709a7f3998
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-12-15 21:19:52 +0100

    Add overlays in glaciers world map.

--- figures/plot/worldmap.py
+++ figures/plot/worldmap.py
@@ -31,34 +31,41 @@ def main():
 
     # initialize figure
     fig = plt.figure(figsize=(12, 6))
-    ax0 = fig.add_axes([0, 0, 0.5, 1], projection=ccrs.Orthographic(
-        central_longitude=0, central_latitude=90))
-    ax1 = fig.add_axes([0.5, 0, 0.5, 1], projection=ccrs.Orthographic(
-        central_longitude=0, central_latitude=-90))
 
     # for each hemisphere
-    for ax in (ax0, ax1):
+    for i in range(2):
+
+        # add axes
+        ax = fig.add_axes([i/2, 0, 0.5, 1], projection=ccrs.Orthographic(
+            central_longitude=0, central_latitude=90*(1-2*i)))
 
         # add physical elements
         cne.add_rivers(ax=ax, edgecolor='0.25', scale='110m')
         cne.add_lakes(ax=ax, edgecolor='0.25', facecolor='0.75', scale='110m')
         cne.add_coastline(ax=ax, edgecolor='0.25', scale='110m')
-        draw_shapefile_nodups('../../data/external/lgm_simple.shp',
-                              ax=ax, alpha=0.75, facecolor='C1')
-        cne.add_glaciers(ax=ax, edgecolor='none', facecolor='C0', scale='110m')
+        cne.add_glaciers(ax=ax, edgecolor='none', facecolor='C0', scale='50m',
+                         zorder=2)
         cne.add_graticules(ax=ax, interval=15, scale='110m', zorder=2)
 
         # ax.set_extent does not work well with ortho proj
         ax.set_xlim((-6.5e6, 6.5e6))
         ax.set_ylim((-6.5e6, 6.5e6))
 
+    # save first frame
+    fig.savefig('worldmap-01')
+
+    # add last glacial cycle glaciers
+    for ax in fig.axes:
+        draw_shapefile_nodups('../../data/external/lgm_simple.shp',
+                              ax=ax, alpha=0.75, facecolor='C3', zorder=1)
+
     # add legend
     fig.legend(
-        [mpl.patches.Patch(facecolor=c, alpha=0.75) for c in ['C1', 'C0']],
-        ['Last Glacial Cycle', 'Modern Glaciers'])
+        [mpl.patches.Patch(facecolor=c, alpha=0.75) for c in ['C0', 'C3']],
+        ['Modern Glaciers', 'Last Glacial Cycle'])
 
-    # save
-    fig.savefig('worldmap')
+    # save second frame
+    fig.savefig('worldmap-02')
 
 
 if __name__ == '__main__':
